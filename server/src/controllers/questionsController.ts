1024d6d2896dc5d64ab647f591bbd8d179bf6ee4
squach28 <squach3443@gmail.com>
2025-03-20 08:47:50 -0700

    add endpoint to add questions

--- server/src/controllers/questionsController.ts
+++ server/src/controllers/questionsController.ts
@@ -3,3 +3,10 @@ import express from "express";
 export const getQuestion = (req: express.Request, res: express.Response) => {
   res.status(200).json({ message: "Success" });
 };
+
+export const addQuestions = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  res.status(201).json({ message: "Success" });
+};
--- server/src/routes/questionsRouter.ts
+++ server/src/routes/questionsRouter.ts
@@ -1,6 +1,7 @@
-import { getQuestion } from "../controllers/questionsController";
+import { getQuestion, addQuestions } from "../controllers/questionsController";
 import express from "express";
 
 export const questionsRouter = express.Router();
 
 questionsRouter.get("/", getQuestion);
+questionsRouter.post("/", addQuestions);
