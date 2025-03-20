97cc9b253dd489ee2d1389c6bb07de89fc079f2a
squach28 <squach3443@gmail.com>
2025-03-20 12:51:41 -0700

    setup tanstack

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import { Link } from "@tanstack/react-router";
+
+const Navbar = () => {
+  return (
+    <ul>
+      <li>
+        <Link to="/">w-trivia</Link>
+      </li>
+    </ul>
+  );
+};
+
+export default Navbar;
