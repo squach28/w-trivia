6a3260f0b8093a077f2172d506634193ab73f7bd
squach28 <squach3443@gmail.com>
2025-03-21 13:22:58 -0700

    change styling for options when user selects an option

--- client/src/components/QuestionItem.tsx
+++ client/src/components/QuestionItem.tsx
@@ -1,5 +1,13 @@
 import { Question } from "@/types/Question";
-import { Button, Grid2, Paper, Stack, Typography } from "@mui/material";
+import {
+  Button,
+  Grid2,
+  Paper,
+  Stack,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import React, { useState } from "react";
 
 interface QuestionProps {
   question: Question;
@@ -7,10 +15,16 @@ interface QuestionProps {
 
 const QuestionItem = ({ question }: QuestionProps) => {
   const options = [...question.incorrectAnswers, question.correctAnswer];
+  const [userGuess, setUserGuess] = useState<string | null>(null);
 
   const parseHTML = (html: string) => {
     const parser = new DOMParser().parseFromString(html, "text/html");
     return parser.body.textContent || "";
+  };
+
+  const onOptionClicked = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const option = e.currentTarget.textContent;
+    setUserGuess(option);
   };
 
   return (
@@ -24,7 +38,12 @@ const QuestionItem = ({ question }: QuestionProps) => {
           <Typography textAlign="center" variant="h6">
             {parseHTML(question.question)}
           </Typography>
-          <QuestionOptions options={options} />
+          <QuestionOptions
+            options={options}
+            onOptionClicked={onOptionClicked}
+            userGuess={userGuess}
+            correctAnswer={question.correctAnswer}
+          />
         </Stack>
       </Paper>
     </article>
@@ -33,14 +52,48 @@ const QuestionItem = ({ question }: QuestionProps) => {
 
 interface QuestionOptionsProps {
   options: Array<string>;
+  onOptionClicked: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  userGuess: null | string;
+  correctAnswer: string;
 }
 
-const QuestionOptions = ({ options }: QuestionOptionsProps) => {
+const QuestionOptions = ({
+  options,
+  onOptionClicked,
+  userGuess,
+  correctAnswer,
+}: QuestionOptionsProps) => {
+  const theme = useTheme();
+
   return (
     <Grid2 container spacing={2}>
       {options.map((option) => (
         <Grid2 key={option} size={{ xs: 12, sm: 12, md: 6 }}>
-          <Button fullWidth variant="outlined">
+          <Button
+            fullWidth
+            variant="outlined"
+            onClick={onOptionClicked}
+            disabled={userGuess !== null}
+            color={
+              userGuess === null
+                ? "primary"
+                : option === correctAnswer
+                  ? "success"
+                  : "error"
+            }
+            sx={{
+              ":disabled": {
+                borderColor:
+                  option === correctAnswer
+                    ? theme.palette.success.main
+                    : theme.palette.error.main,
+                color:
+                  option === correctAnswer
+                    ? theme.palette.success.main
+                    : theme.palette.error.main,
+              },
+            }}
+          >
             {option}
           </Button>
         </Grid2>
