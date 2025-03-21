21318653036a3d22dfacda4a3208a81482bfa45a
squach28 <squach3443@gmail.com>
2025-03-21 08:37:06 -0700

    create uuid in local storage if it doesn't exist

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,9 +1,17 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 export const Route = createFileRoute("/")({
-  component: RouteComponent,
+  component: HomeComponent,
 });
 
-function RouteComponent() {
-  return <div>Hello "/"!</div>;
+function HomeComponent() {
+  useEffect(() => {
+    const id = localStorage.getItem("uuid");
+    if (!id) {
+      localStorage.setItem("uuid", uuidv4());
+    }
+  }, []);
+  return <main></main>;
 }
