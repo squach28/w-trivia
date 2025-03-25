0e4a6328609f2c92049d6746f1da5d34e81a9044
squach28 <squach3443@gmail.com>
2025-03-25 15:02:33 -0700

    add db connection

---
+++ server/src/db/db.ts
@@ -0,0 +1,12 @@
+import { Pool } from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const db = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: Number(process.env.DB_PORT),
+});
